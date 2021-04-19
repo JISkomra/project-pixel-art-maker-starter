abfd8aea6ed151a76b1d982e824f6f4e3ccef590
Jesse Skomra <jiskomra@gmail.com>
2021-04-19 19:59:12 -0400

    import javascript file

--- designs.js
+++ designs.js
@@ -1,10 +1,15 @@
-// Select color input
-// Select size input
-
+<input id="colorPicker" type="color">// Select color input
+<form id="sizePicker">
+    <p>Grid Height:</p>
+    <input id="inputHeight" type="number" name="height" min="1" value="1"></input>
+    <p>Grid Width:</p>
+    <input id="inputWidth" type="number" name="width" min="1" value="1"></input>
+    <input type="submit"></input>
+</form>
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
+
 
 }
