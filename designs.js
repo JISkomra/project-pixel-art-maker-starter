923e6e95aecde58a7bc0ffab0effaa719ef772d2
Jesse Skomra <jiskomra@gmail.com>
2021-04-22 11:04:22 -0400

    took away ul height and width and made conditional for number of boxes

--- designs.js
+++ designs.js
@@ -1,15 +1,50 @@
-<input id="colorPicker" type="color">// Select color input
-<form id="sizePicker">
-    <p>Grid Height:</p>
-    <input id="inputHeight" type="number" name="height" min="1" value="1"></input>
-    <p>Grid Width:</p>
-    <input id="inputWidth" type="number" name="width" min="1" value="1"></input>
-    <input type="submit"></input>
-</form>
+// Select color input
+
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function newLi(){
+    return document.createElement("li");  
+}
 
+function newUl(ulName){
+    var newUl = document.createElement("ul");
+    newUl.id = ulName;
+    return newUl;
+}
 
+function makeGrid(){
+
+    var height = document.getElementById("inputHeight").value;
+    var width = document.getElementById("inputWidth").value;
+    var intWidth = parseInt(width);
+    var intHeight = parseInt(height);
+
+    //if there is one, remove it
+    var gridUl = document.getElementsByTagName("ul");
+    if(gridUl.length > 0){
+        //remove it, then create and append to body
+        gridUl[0].remove(); 
+        var gridUl = newUl("grid");
+    }
+
+    //create grid
+    var gridUl = newUl("grid");
+    
+    if (intHeight <= 20 & intWidth <=20 ){
+        for(i = 0; i < intHeight; i++) {
+            for(var n = 0; n < intWidth; n++) {
+            gridUl.appendChild(newLi());
+            }
+        }
+    }else{
+        makeGrid();
+    }
+
+    //append grid
+    document.body.appendChild(gridUl);
+
+    //shrink grid depending on amount of boxes
+    document.getElementById("ul_o").getElementsByTagName("li").length
 
 }
+
