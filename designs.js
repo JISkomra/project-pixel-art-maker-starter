9ae00948445491506f95f9984525de3fe69ca12f
Jesse Skomra <jiskomra@gmail.com>
2021-04-23 19:55:14 -0400

    finalized project

--- designs.js
+++ designs.js
@@ -1,17 +1,3 @@
-// Select color input
-
-// When size is submitted by the user, call makeGrid()
-
-function newLi(){
-    return document.createElement("li");  
-}
-
-function newUl(ulName){
-    var newUl = document.createElement("ul");
-    newUl.id = ulName;
-    return newUl;
-}
-
 function makeGrid(){
 
     var height = document.getElementById("inputHeight").value;
@@ -19,32 +5,30 @@ function makeGrid(){
     var intWidth = parseInt(width);
     var intHeight = parseInt(height);
 
-    //if there is one, remove it
-    var gridUl = document.getElementsByTagName("ul");
-    if(gridUl.length > 0){
-        //remove it, then create and append to body
-        gridUl[0].remove(); 
-        var gridUl = newUl("grid");
-    }
-
-    //create grid
-    var gridUl = newUl("grid");
+    var theTable = "";
     
+    //create the table
     if (intHeight <= 20 & intWidth <=20 ){
         for(i = 0; i < intHeight; i++) {
-            for(var n = 0; n < intWidth; n++) {
-            gridUl.appendChild(newLi());
+            theTable += "<tr>";
+            for(n = 0; n < intWidth; n++) {
+                theTable += "<td></td>"
             }
+            theTable += "</tr>";
         }
     }else{
         makeGrid();
     }
 
-    //append grid
-    document.body.appendChild(gridUl);
+    //set the table
+    document.getElementById("pixelCanvas").innerHTML = theTable;
 
-    //shrink grid depending on amount of boxes
-    document.getElementById("ul_o").getElementsByTagName("li").length
-
+    //add event listeners to pixels
+    var allTds = document.getElementsByTagName("td"); 
+    for (i = 0; i < allTds.length; i++) {
+        allTds[i].addEventListener("click", function addColor(){
+            var colorInput = document.getElementById("colorPicker").value;
+            this.style.backgroundColor = colorInput;
+        });
+    }
 }
-
